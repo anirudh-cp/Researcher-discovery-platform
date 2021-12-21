a9063fd9a795d3bf0d9e0c9e32e860185bd410c0
anirudh-cp <74400035+anirudh-cp@users.noreply.github.com>
2021-12-21 14:24:33 +0530

    ACM scrapers and Comments
    ACM website search results and page scrapers done. Testing seems to indicate they work fine as well. They have been integrated in main to create a new workbook. Comments have been added to the 'scraper.py' file for added readability.

--- main.py
+++ main.py
@@ -7,25 +7,45 @@ def main():
     # term = ['Apple', 'ice']
 
     work_book = WorkBook(encoding='utf-8')
-    work_book.add_sheet('Results', ['Title', 'URL'])
 
+    # Google Scholar
+    work_book.add_sheet('Google Scholar Source', ['Title', 'URL'])
     results, sci_dir = scrape_scholar(term)
 
     for index, term in enumerate(results):
-        work_book.sheet['Results'].write_line(index+1, term)
+        work_book.sheet['Google Scholar Source'].write_line(index+1, term)
 
-    work_book.add_sheet('People', ['First Name', 'Last Name',
+    # ----------------------------------------
+
+    # Science Direct
+    work_book.add_sheet('Science Direct', ['First Name', 'Last Name',
                                    'Qualification', 'URL'])
 
-    for res in sci_dir:
-        print(res)
+    #for res in sci_dir:
+    #    print(res)
 
     line = 1
     for res in sci_dir:
         people = scrape_sci_dir(res[1])
         for person in people:
             print(person)
-            work_book.sheet['People'].write_line(line, person)
+            work_book.sheet['Science Direct'].write_line(line, person)
+            line += 1
+
+    # ----------------------------------------
+
+    # ACM
+    results = scrape_ACM_source(term)
+    work_book.add_sheet('ACM', ['First Name', 'Last Name',
+                                           'Qualification', 'URL'])
+
+    line = 1
+    for url in results:
+        title, people = scrape_ACM_page(url[0])
+        # print(title)
+        for person in people:
+            # print(person)
+            work_book.sheet['ACM'].write_line(line, person)
             line += 1
 
     work_book.save('Results.xls')
@@ -56,7 +76,16 @@ def main_process(term):
     return data
 
 
-'''
+def main_():
+    #term = input('Enter search term : ').split()
+    term = ['machine', 'learning']
+    results = scrape_ACM_source(term)
+    for url in results:
+        title, people = scrape_ACM_page(url[0])
+        print(title)
+        for person in people:
+            print(person)
+
+
 if __name__ == '__main__':
     main()
-'''
--- scraper.py
+++ scraper.py
@@ -1,17 +1,43 @@
-import requests
-# import lxml
-from bs4 import BeautifulSoup
+"""
+Module for Scrapers and associated helper functions.
 
+Available scrapers:
+-------------------
+scrape_scholar      : Google Scholar    :   Get all title and URL and titles
+                                            and URL for Science Direct pages.
+
+scrape_sci_dir_page : Science Direct    :   Get first name, last name,
+                                            qualifications and SCOPUS query
+                                            link for each author.
+
+scrape_ACM_source   : ACM               :   Get URLs from ACM search results.
+
+scrape_ACM_page     : ACM               :   Get first name, last name,
+                                            qualifications and ACM profile
+                                            link for each author.
+"""
+
+# For scrapers
+import requests
+from bs4 import BeautifulSoup
+# import lxml
+
+# For helper functions
 import re
 from urllib.parse import quote
 
 
 def sanitize(text):
+    """ Convert UTF-8 text to URL friendly string. """
+
     data = '+'.join(quote(x) for x in text.split())
     return data
 
 
 def get_uni(qualification, ref):
+    """ Check through all qualifications and return with university term
+    for the SCOPUS query. """
+
     for term in ref:
         text = qualification[term]
         data = text.split(',')
@@ -21,6 +47,8 @@ def get_uni(qualification, ref):
 
 
 def get_ref(author):
+    """ Get references of author to link to appropriate university. """
+
     res = []
     for auth in author.find_all("span", {"class": "author-ref"}):
         val = auth.get_text(strip=True)
@@ -30,6 +58,8 @@ def get_ref(author):
 
 
 def get_qualifications(qualification, ref):
+    """ Return string with all qualifications. """
+
     out = ''
     for term in ref:
         out = out + qualification[term] + '; '
@@ -37,25 +67,34 @@ def get_qualifications(qualification, ref):
 
 
 def scrape_scholar(term):
+    """ Scrape through Google Scholar. Return all results and results
+    to the Science Direct Website. """
+
+    # Create URL to scrape
     term = '+'.join(x for x in term)
-    print(term)
+    # print(term)
     url = f'https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5' \
           f'&q={term}&btnG=&oq=app'
-    print(url)
 
+    # print(url)
+
+    # Impersonate user.
     headers_ = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) '
                               'AppleWebKit/537.36 (KHTML, like Gecko) '
                               'Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'}
 
+    # Get data via request
     req = requests.get(url, headers=headers_)
     soup = BeautifulSoup(req.content, 'lxml')
 
+    # Get records page div
     records_page = soup.find("div", {"id": "gs_res_ccl_mid"})
     records = records_page.find_all("div", {"class": "gs_r gs_or gs_scl"})
 
     results = []
     sci_dir = []
 
+    # Scrape every result in the div and get required data.
     for record_wrapper in records:
         record = record_wrapper.find("div", {"class": "gs_ri"})
 
@@ -64,6 +103,7 @@ def scrape_scholar(term):
         title = title_data.get_text(strip=True)
         record_url = title_data['href']
 
+        # Place data in appropriate lists.
         results.append([title, record_url])
         if record_url.split('.')[1] == 'sciencedirect':
             sci_dir.append([title, record_url])
@@ -72,13 +112,21 @@ def scrape_scholar(term):
 
 
 # noinspection PyPep8
-def scrape_sci_dir(url):
+def scrape_sci_dir_page(url):
+    """ Scrape Science Direct page for first name, last name, qualifications
+    and SCOPUS query link for each author. """
+
+    # Impersonate user
     headers_ = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) '
                               'AppleWebKit/537.36 (KHTML, like Gecko) '
                               'Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'}
 
+    # Get data via request
     req = requests.get(url, headers=headers_)
     soup = BeautifulSoup(req.content, 'lxml')
+
+    # This part is to map the qualifications to each author.
+    # Take the data by parsing the javascript code.
 
     script = soup.find('script', {"type": "application/json"})
     script = str(script)
@@ -88,6 +136,7 @@ def scrape_sci_dir(url):
     data_full = re.findall(pattern, script, re.M)
     data_full = set(data_full)
 
+    # Map a reference character to each qualification as shown in the page.
     qualification = {}
     for term in data_full:
         data = term.split('},{')
@@ -97,21 +146,25 @@ def scrape_sci_dir(url):
                 data[1].split(':')[-1].replace('"', '').replace('}', '')
         else:
             # noinspection PyPep8
-            qualification['0'] = data[0].split(':')[-1].\
+            qualification['0'] = data[0].split(':')[-1]. \
                 replace('"', '').replace('}', '')
 
     # print(qualification)
 
+    # Start scraping the author window.
     author_group = soup.find("div", {"class": "AuthorGroups text-xs"})
 
     author_details = author_group.find_all("a", {"class": "author size-m "
                                                           "workspace-trigger"})
     results = []
 
+    # For every author get details and map qualifications. If any of the
+    # qualifications have the term 'university' in them,
+    # include in the SCOPUS query.
     for author in author_details:
-        given_name = author.find("span", {"class": "text given-name"}).\
+        given_name = author.find("span", {"class": "text given-name"}). \
             get_text(strip=True)
-        surname = author.find("span", {"class": "text surname"}).\
+        surname = author.find("span", {"class": "text surname"}). \
             get_text(strip=True)
 
         if len(qualification) > 1:
@@ -136,3 +189,101 @@ def scrape_sci_dir(url):
                         get_qualifications(qualification, ref), url])
 
     return results
+
+
+def scrape_ACM_source(term):
+    """ Scrape the ACM search results page. """
+
+    # Create URL to scrape.
+    term = '+'.join(x for x in term)
+    # print(term)
+    url = f'https://dl.acm.org/action/doSearch?AllField={term}&startPage=0&ContentItemType=research-article'
+
+    # print(url)
+
+    # Impersonate user
+    headers_ = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) '
+                              'AppleWebKit/537.36 (KHTML, like Gecko) '
+                              'Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'}
+
+    # Get data via request
+    req = requests.get(url, headers=headers_)
+    soup = BeautifulSoup(req.content, 'lxml')
+
+    # Find where all records are in the div
+    records_page = soup.find("div", {"class": "search-result doSearch"})
+    records = records_page.find_all("li", {
+        "class": "search__item issue-item-container"})
+
+    results = []
+
+    # Scrape through every record and get the URL alone.
+    for record_wrapper in records:
+        record = record_wrapper.find("div", {
+            "class": "issue-item issue-item--search clearfix"})
+
+        title_heading = record.find("span", {"class": "hlFld-Title"})
+        title_data = title_heading.find("a")
+        record_url = title_data['href']
+
+        results.append(['https://dl.acm.org' + record_url])
+
+    return results
+
+
+def scrape_ACM_page(url):
+    """ Scrape a ACM paper page and get first name, last name,
+    qualifications, ACM profile page link. """
+
+    # Impersonate user.
+    headers_ = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) '
+                              'AppleWebKit/537.36 (KHTML, like Gecko) '
+                              'Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'}
+
+    # Get data via request.
+    req = requests.get(url, headers=headers_)
+    soup = BeautifulSoup(req.content, 'lxml')
+
+    # Get title of research paper.
+    title = soup.find("h1", {"class": "citation__title"}).get_text(strip=True)
+
+    # Get author details div
+    results = []
+    authors_wrapper = soup.find("div", {"id": "sb-1"})
+    authors = authors_wrapper.find_all("li", {"class": "loa__item"})
+
+    # For every author get the name. Then try for qualifications.
+    # If the information paragraph exists, get qualification and URL.
+    # If the information paragraph does not exist, get URL alone from
+    # hidden box and leave qualifications blank.
+    # If the information paragraph exists with no URL, get
+    # qualification and set URL to empty string.
+
+    for author in authors:
+        name = author.find("a")['title']
+        info = author.find("span", {"class": "loa_author_inst"}).find("p")
+        try:
+            # Try to get info from paragraph tag
+            qualification = info.get_text(strip=True)
+            url = 'https://dl.acm.org/profile/' + info['data-doi'].split('-')[
+                1]
+        except AttributeError:
+            # If paragraph does not exist, text and url cannot be found directly
+            qualification = ''
+            url = 'https://dl.acm.org' + \
+                  author.find("div", {"class": "author-info__body"}).find("a")[
+                      'href']
+        except KeyError:
+            # Paragraph block does exist, text can be found, url key is missing
+            qualification = info.get_text(strip=True)
+            url = ''
+
+        try:
+            first_name, last_name = name.split(' ', 1)[0], name.split(' ', 1)[
+                1]
+        except IndexError:
+            first_name, last_name = name, ''
+
+        results.append([first_name, last_name, qualification, url])
+
+    return title, results
